f23f085c80b676033eae64be856844a5ade36776
Monika Patel <monikapatel.it@outlook.com>
2018-10-07 12:13:11 +1300

    Updates

---
+++ precache-manifest.4e776e2fda3f3e62f8ba92143cf109fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CREATE-REACT-APP-Hello/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b186de2d9c9e87200754",
+    "url": "/CREATE-REACT-APP-Hello/static/js/runtime~main.b186de2d.js"
+  },
+  {
+    "revision": "633e25481f4bc8c5193b",
+    "url": "/CREATE-REACT-APP-Hello/static/js/main.633e2548.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/CREATE-REACT-APP-Hello/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "633e25481f4bc8c5193b",
+    "url": "/CREATE-REACT-APP-Hello/static/css/main.2a438a31.chunk.css"
+  },
+  {
+    "revision": "3ba462d6aba76c53ded2f856b7e20f8e",
+    "url": "/CREATE-REACT-APP-Hello/index.html"
+  }
+];
